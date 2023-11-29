0d1121015d15bfa9ddcd2a56108b43dfc0c6c881
ainp <luijinjian@yeah.net>
2023-11-29 11:13:44 +0800

    Add files via upload
    Signed-off-by: ainp <luijinjian@yeah.net>

--- virtio.bat
+++ virtio.bat
@@ -1,47 +1,47 @@
-@echo.Welcome to the virtio driver image creator!
+@echo.Welcome to the virtio driver  image creator!
 
 @rem pre-cleanup of temp dirs
-rd /s /q C:\ainpmnt 2>NUL
+rd /s /q .\mnt 2>NUL
 
-mkdir C:\ainpmnt
+mkdir mnt
 
 @rem add driver to boot.wim index 1
-dism /get-wiminfo /wimfile:C:\iso\tinier11\sources\boot.wim
+dism /get-wiminfo /wimfile:.\tinier11\sources\boot.wim
 
-dism /mount-wim /wimfile:C:\iso\tinier11\sources\boot.wim /index:1 /mountdir:C:\ainpmnt
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\Balloon\w11\amd64\balloon.inf /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\NetKVM\w11\amd64\netkvm.inf  /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\viostor\w11\amd64\viostor.inf /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\vioscsi\w11\amd64\vioscsi.inf /forceunsigned
+dism /mount-wim /wimfile:.\tinier11\sources\boot.wim /index:1 /mountdir:.\mnt
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\Balloon\w11\amd64\balloon.inf /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\NetKVM\w11\amd64\netkvm.inf  /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\viostor\w11\amd64\viostor.inf /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\vioscsi\w11\amd64\vioscsi.inf /forceunsigned
 
-dism /image:C:\ainpmnt /get-drivers
-dism /unmount-wim /mountdir:C:\ainpmnt /commit
+dism /image:.\mnt /get-drivers
+dism /unmount-wim /mountdir:.\mnt /commit
 
 
 @rem add driver to boot.wim index 2
-dism /get-wiminfo /wimfile:C:\iso\tinier11\sources\boot.wim
+dism /get-wiminfo /wimfile:.\tinier11\sources\boot.wim
 
-dism /mount-wim /wimfile:C:\iso\tinier11\sources\boot.wim /index:2 /mountdir:C:\ainpmnt
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\Balloon\w11\amd64\balloon.inf /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\NetKVM\w11\amd64\netkvm.inf  /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\viostor\w11\amd64\viostor.inf /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\vioscsi\w11\amd64\vioscsi.inf /forceunsigned
+dism /mount-wim /wimfile:.\tinier11\sources\boot.wim /index:2 /mountdir:.\mnt
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\Balloon\w11\amd64\balloon.inf /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\NetKVM\w11\amd64\netkvm.inf  /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\viostor\w11\amd64\viostor.inf /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\vioscsi\w11\amd64\vioscsi.inf /forceunsigned
 
-dism /image:C:\ainpmnt /get-drivers
-dism /unmount-wim /mountdir:C:\ainpmnt /commit
+dism /image:.\mnt /get-drivers
+dism /unmount-wim /mountdir:.\mnt /commit
 
 
 @rem add driver to install.wim
-dism /get-wiminfo /wimfile:C:\iso\tinier11\sources\install.wim
+dism /get-wiminfo /wimfile:.\tinier11\sources\install.wim
 
-dism /mount-wim /wimfile:C:\iso\tinier11\sources\install.wim /index:1 /mountdir:C:\ainpmnt
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\Balloon\w11\amd64\balloon.inf /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\NetKVM\w11\amd64\netkvm.inf  /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\viostor\w11\amd64\viostor.inf /forceunsigned
-dism /image:C:\ainpmnt /add-driver /driver:C:\iso\virtio\vioscsi\w11\amd64\vioscsi.inf /forceunsigned
+dism /mount-wim /wimfile:.\tinier11\sources\install.wim /index:1 /mountdir:.\mnt
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\Balloon\w11\amd64\balloon.inf /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\NetKVM\w11\amd64\netkvm.inf  /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\viostor\w11\amd64\viostor.inf /forceunsigned
+dism /image:.\mnt /add-driver /driver:.\virtio-win-0.1.240\vioscsi\w11\amd64\vioscsi.inf /forceunsigned
 
-dism /image:C:\ainpmnt /get-drivers
-dism /unmount-wim /mountdir:C:\ainpmnt /commit
-rd /s /q C:\ainpmnt 2>NUL
+dism /image:.\mnt /get-drivers
+dism /unmount-wim /mountdir:.\mnt /commit
+rd /s /q .\mnt 2>NUL
 
 @echo.The virtio driver image success!
